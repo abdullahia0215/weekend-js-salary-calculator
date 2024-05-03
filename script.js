90c09137ebdbe2555b6747a4d2a8af9bf5b6fa24
Abdi <abdullahia0215@gmail.com>
2024-05-03 15:58:13 -0500

    imported html elements into script, set up stylesheet

---
+++ script.js
@@ -0,0 +1,33 @@
+onReady();
+
+
+function onReady(){
+    console.log('java is ready')
+}
+
+
+
+
+
+let firstName = document.getElementById("first-name")
+let lastName = document.getElementById("last-name")
+let employeeID = document.getElementById("employee-id")
+
+let employeeTitle = document.getElementById("employee-title")
+let employeeSalary = document.getElementById("employee-salary")
+
+let employeeTable = document.getElementById("employee-table")
+
+
+
+function addEmployeeData (event){
+    event.preventDefault();
+    employeeTable.innerHTML += `<tr>
+    <td>${firstName.value}</td>
+    <td>${lastName.value}</td>
+    <td>${employeeID.value}</td>
+    <td>${employeeTitle.value}</td>
+    <td>${employeeSalary.value}</td>
+    </tr>`
+
+}
