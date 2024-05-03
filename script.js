15c763236b8da265eda6b84f2cda51fb10f1b9fa
Abdi <abdullahia0215@gmail.com>
2024-05-03 16:42:58 -0500

    salaries are counted up in the global totalSalaries variable

--- script.js
+++ script.js
@@ -7,21 +7,23 @@ function onReady(){
 
 
 
-
+let totalSalaries = 0;
 
 let firstName = document.getElementById("first-name")
 let lastName = document.getElementById("last-name")
 let employeeID = document.getElementById("employee-id")
 
 let employeeTitle = document.getElementById("employee-title")
-let employeeSalary = document.getElementById("employee-salary")
+let employeeSalary = (document.getElementById("employee-salary"));
 
 let employeeTable = document.getElementById("employee-table")
 
 
 
 function addEmployeeData (event){
-    event.preventDefault();
+    event.preventDefault();     
+    totalSalaries += Number(employeeSalary.value);
+    console.log(employeeSalary.value)
     employeeTable.innerHTML += `<tr>
     <td>${firstName.value}</td>
     <td>${lastName.value}</td>
@@ -30,10 +32,13 @@ function addEmployeeData (event){
     <td>${employeeSalary.value}</td>
     </tr>`
 
-    firstName.value='';
-    lastName.value='';
-    employeeID.value='';
-    employeeTitle.value='';
-    employeeSalary.value='';
 
+
+    // firstName.value='';
+    // lastName.value='';
+    // employeeID.value='';
+    // employeeTitle.value='';
+    // employeeSalary.value='';
+    console.log(totalSalaries)
+   
 }
