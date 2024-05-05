04b1dafa21792088e24570fd05b5433ba240c20a
Abdi <abdullahia0215@gmail.com>
2024-05-05 13:18:15 -0500

    formatted

--- script.js
+++ script.js
@@ -1,63 +1,51 @@
-
-
 let totalSalaries = 0;
 
-let firstName = document.getElementById("first-name")
-let lastName = document.getElementById("last-name")
-let employeeID = document.getElementById("employee-id")
+let firstName = document.getElementById("first-name");
+let lastName = document.getElementById("last-name");
+let employeeID = document.getElementById("employee-id");
 
-let employeeTitle = document.getElementById("employee-title")
-let employeeSalary = (document.getElementById("employee-salary"));
+let employeeTitle = document.getElementById("employee-title");
+let employeeSalary = document.getElementById("employee-salary");
 
-let employeeTable = document.getElementById("employee-table")
-let salaryDiv = document.getElementById("totalSalariesDiv")
+let employeeTable = document.getElementById("employee-table");
+let salaryDiv = document.getElementById("totalSalariesDiv");
 
 onReady();
 
-
-function onReady(){
-    console.log('java is ready')
-    displayTotalSalaries();
+function onReady() {
+  console.log("java is ready");
+  displayTotalSalaries();
 }
 
-
-
-
-
-
-
-function addEmployeeData (event){
-    event.preventDefault();     
-    totalSalaries += Number(employeeSalary.value);
-    console.log(employeeSalary.value)
-    employeeTable.innerHTML += `<tr>
+function addEmployeeData(event) {
+  event.preventDefault();
+  totalSalaries += Number(employeeSalary.value);
+  console.log(employeeSalary.value);
+  employeeTable.innerHTML += `<tr>
     <td>${firstName.value}</td>
     <td>${lastName.value}</td>
     <td>${employeeID.value}</td>
     <td>${employeeTitle.value}</td>
     <td>${employeeSalary.value}</td>
     <td><button onClick="removeEmployeeData(event)">Delete</button></td>
-    </tr>`
-    // firstName.value='';
-    // lastName.value='';
-    // employeeID.value='';
-    // employeeTitle.value='';
-    // employeeSalary.value='';
-    console.log(totalSalaries)
-    displayTotalSalaries(event);
-    
+    </tr>`;
+  // firstName.value='';
+  // lastName.value='';
+  // employeeID.value='';
+  // employeeTitle.value='';
+  // employeeSalary.value='';
+  console.log(totalSalaries);
+  displayTotalSalaries(event);
 }
 
 function removeEmployeeData(event) {
-    const row = event.target.closest('tr');
-    const salaryToRemove = Number(row.children[4].textContent); 
-    row.remove();
-    totalSalaries -= salaryToRemove;
-    displayTotalSalaries();
+  const row = event.target.closest("tr");
+  const salaryToRemove = Number(row.children[4].textContent);
+  row.remove();
+  totalSalaries -= salaryToRemove;
+  displayTotalSalaries();
 }
 
-
-function displayTotalSalaries (){
-    salaryDiv.innerHTML = `<p>Total Monthly: ${totalSalaries}</p>`
+function displayTotalSalaries() {
+  salaryDiv.innerHTML = `<p>Total Monthly: ${totalSalaries}</p>`;
 }
-
